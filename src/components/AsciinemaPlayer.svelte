bd1119ec201c75e6ab2ef49e82a8706437b3afd1
Miguel Guthridge <hello@miguelguthridge.com>
2024-05-19 18:22:12 +1000

    Fix type checks

--- src/components/AsciinemaPlayer.svelte
+++ src/components/AsciinemaPlayer.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import { onMount } from 'svelte';
     import 'asciinema-player/dist/bundle/asciinema-player.css';
+    // asciinema doesn't have a types module
+    // @ts-expect-error
     import * as AsciinemaPlayer from 'asciinema-player';
 
     /** URL of the Asciinema recording */
