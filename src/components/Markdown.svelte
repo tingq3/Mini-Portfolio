b0ea28dfc55b67cab083dce16ac0f5ba26d518c0
Miguel Guthridge <hello@miguelguthridge.com>
2024-05-25 23:56:25 +1000

    Markdown rendering: enable syntax highlighting

--- src/components/Markdown.svelte
+++ src/components/Markdown.svelte
@@ -1,41 +1,50 @@
 <script lang="ts">
-    export let source: string;
+import { marked } from 'marked';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/default.css';
+import { onMount } from 'svelte';
 
-    import { marked } from 'marked';
-    // https://github.com/markedjs/marked/discussions/2982#discussioncomment-6979586
-    const renderer = {
-      link(href: string, title: string | null | undefined, text: string) {
-        const link = marked.Renderer.prototype.link.call(this, href, title, text);
-        return link.replace('<a', "<a target='_blank' rel='noreferrer' ");
-      }
-    };
-    marked.use({ renderer });
+export let source: string;
+
+// https://github.com/markedjs/marked/discussions/2982#discussioncomment-6979586
+const renderer = {
+  link(href: string, title: string | null | undefined, text: string) {
+    const link = marked.Renderer.prototype.link.call(this, href, title, text);
+    return link.replace('<a', "<a target='_blank' rel='noreferrer' ");
+  }
+};
+marked.use({ renderer });
+
+onMount(() => {
+  hljs.highlightAll();
+});
 </script>
 
 <div class="markdown-render">
-    <!--
-        We only render markdown specifically from the `data/` directory, so
-        this is safe.
-    -->
-    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-    {@html marked(source)}
+  <!--
+    We only render markdown specifically from the `data/` directory, so
+    this is safe.
+  -->
+  <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+  {@html marked(source)}
 </div>
 
 <style>
-    .markdown-render :global(code) {
-        background-color: rgb(245, 245, 245);
-        padding: 2px 5px;
-        border-radius: 3px;
-        border-color: rgb(231, 231, 231);
-        border-style: solid;
-        border-width: 1px;
-        font-weight: bold;
-    }
+  .markdown-render :global(p > code), .markdown-render :global(pre) {
+    background-color: rgb(245, 245, 245);
+    padding: 2px 5px;
+    border-radius: 3px;
+    border-color: rgb(231, 231, 231);
+    border-style: solid;
+    border-width: 1px;
+    /* Kinda bold but not obnoxiously so */
+    font-weight: 600;
+  }
 
-    .markdown-render :global(a) {
-        text-decoration: none;
-    }
-    .markdown-render :global(a):hover {
-        text-decoration: underline;
-    }
+  .markdown-render :global(a) {
+    text-decoration: none;
+  }
+  .markdown-render :global(a):hover {
+    text-decoration: underline;
+  }
 </style>
