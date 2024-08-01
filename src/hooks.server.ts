94bcd6f689163ab3d08f2676b765be685109db88
Maddy Guthridge <hello@maddyguthridge.com>
2024-08-01 19:56:25 +1000

    Add logging middleware in dev mode

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,13 @@
-// This file runs on app start-up...... well,,, kinda
-// it runs on the first request in dev mode, which is super duper annoying
-// since it means our data doesn't get validated until we've waited a good 5
-// seconds for the server to start up.
-import { reloadData } from '$lib/server';
+import { sequence } from '@sveltejs/kit/hooks';
+import type { Handle } from '@sveltejs/kit';
+import { dev } from '$app/environment';
+import { logger } from './middleware/logger';
 
-// reloadData();
+
+const middleware: Handle[] = [];
+
+if (dev) {
+  middleware.push(logger);
+}
+
+export const handle = sequence(...middleware);
---
+++ src/middleware/logger.ts
@@ -0,0 +1,71 @@
+import type { Handle } from '@sveltejs/kit';
+import chalk, { type ChalkInstance } from 'chalk';
+
+/** Colorize request method for pretty-printing */
+function colorRequestMethod(method: string): string {
+  const mapping: Record<string, ChalkInstance> = {
+    'get': chalk.blue,
+    'post': chalk.green,
+    'put': chalk.yellow,
+    'delete': chalk.red
+  }
+
+  return mapping[method.toLowerCase()](method) ?? method;
+}
+
+/** Format time to handle request, given a start time */
+function formatDuration(start: number, end: number) {
+  const duration = end - start;
+  let color: ChalkInstance;
+  if (duration < 10) {
+    color = chalk.gray;
+  } else if (duration < 50) {
+    color = chalk.white;
+  } else if (duration < 100) {
+    color = chalk.green;
+  } else if (duration < 500) {
+    color = chalk.blue;
+  } else if (duration < 1000) {
+    color = chalk.yellow;
+  } else {
+    color = chalk.red;
+  }
+
+  return color(`(${duration}ms)`);
+}
+
+/** Colorize status code */
+function colorStatus(code: number): string {
+  if (code < 300) {
+    return chalk.green(code);
+  } else if (code < 400) {
+    return chalk.blue(code);
+  } else if (code < 500) {
+    return chalk.yellow(code);
+  } else {
+    return chalk.red(code);
+  }
+}
+
+/**
+ * Logging middleware to display incoming requests
+ *
+ * Shows in the format:
+ *
+ *     2022-10-01T14:48:00.000Z GET / (23ms) 200
+ *
+ * Adapted from: https://www.reddit.com/r/sveltejs/comments/xtbkpb
+ */
+export const logger: Handle = async ({ event, resolve }) => {
+  const requestStartTime = Date.now();
+  const response = await resolve(event);
+
+  console.log(
+    new Date(requestStartTime).toISOString(),
+    colorRequestMethod(event.request.method),
+    chalk.magenta(event.url.pathname),
+    formatDuration(requestStartTime, Date.now()),
+    colorStatus(response.status)
+  );
+  return response;
+};
