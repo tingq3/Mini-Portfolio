2d136888f83f673ffc1d185997ac18a95647fa8e
Maddy Guthridge <hello@maddyguthridge.com>
2024-08-03 21:06:58 +1000

    Implement password changing

--- src/routes/api/admin/auth/change/+server.ts
+++ src/routes/api/admin/auth/change/+server.ts
@@ -1,5 +1,50 @@
+import { hashAndSalt, revokeSession, validateToken } from '$lib/server/auth';
+import { dataDirIsInit } from '$lib/server/data/dataDir';
+import { getLocalConfig, setLocalConfig } from '$lib/server/data/localConfig.js';
 import { error, json } from '@sveltejs/kit';
+import { nanoid } from 'nanoid';
+import validator from 'validator';
 
 export async function POST({ request, cookies }) {
-  return json({}, { status: 418 });
+  const token = request.headers.get('Authorization');
+  if (!token) {
+    return error(401, 'Authorization token is required');
+  }
+
+  if (!await dataDirIsInit()) {
+    return error(400, 'Server is not initialized');
+  }
+
+  try {
+    await validateToken(token);
+  } catch (e) {
+    return error(401, `${e}`);
+  }
+
+  const local = await getLocalConfig();
+
+  if (!local.auth) {
+    throw Error('Unreachable');
+  }
+
+  const { oldPassword, newPassword } = await request.json();
+
+  if (hashAndSalt(local.auth.password.salt, oldPassword) !== local.auth.password.hash) {
+    return error(403, 'Old password is incorrect');
+  }
+
+  if (!validator.isStrongPassword(newPassword)) {
+    return error(400, 'New password is not strong enough');
+  }
+
+  // Hash and salt new password
+  const salt = nanoid();
+  const hash = hashAndSalt(salt, newPassword);
+  local.auth.password = {
+    hash,
+    salt,
+  };
+  await setLocalConfig(local);
+
+  return json({}, { status: 200 });
 }
--- tests/backend/admin/auth/change.test.ts
+++ tests/backend/admin/auth/change.test.ts
@@ -4,13 +4,41 @@
  * Allows users to change the auth info, provided they are logged in.
  */
 import { it, expect } from 'vitest';
+import { setup } from '../../helpers';
+import api from '$api';
 
-it.todo('Blocks unauthorized users');
+it('Blocks unauthorized users', async () => {
+  const { password } = await setup();
+  await expect(api.admin.auth.change('', password, 'abc123ABC$'))
+    .rejects.toMatchObject({ code: 401 });
+});
 
-it.todo('Allows users to reset their password');
+it('Allows users to reset their password', async () => {
+  const { token, username, password } = await setup();
+  const newPassword = 'abc123ABC$';
+  await expect(api.admin.auth.change(token, password, newPassword))
+    .resolves.toStrictEqual({});
+  // Logging in with new password should succeed
+  await expect(api.admin.auth.login(username, newPassword)).toResolve();
+  // Logging in with old password should fail
+  await expect(api.admin.auth.login(username, password)).toReject();
+});
 
-it.todo('Rejects non-existent usernames');
+it('Rejects incorrect previous passwords', async () => {
+  const { token } = await setup();
+  await expect(api.admin.auth.change(token, 'incorrect', 'abc123ABC$'))
+    .rejects.toMatchObject({ code: 403 });
+});
 
-it.todo('Rejects incorrect previous passwords');
+it('Rejects insecure new passwords', async () => {
+  const { token, password } = await setup();
+  await expect(api.admin.auth.change(token, password, 'insecure'))
+    .rejects.toMatchObject({ code: 400 });
+});
 
-it.todo('Rejects insecure new passwords');
+it('Errors if the data is not set up', async () => {
+  const { token, password } = await setup();
+  await api.debug.clear();
+  await expect(api.admin.auth.change(token, password, 'abc123ABC$'))
+    .rejects.toMatchObject({ code: 400 });
+});
