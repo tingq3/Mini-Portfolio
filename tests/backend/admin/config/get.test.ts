826945e2313e85b58905efc0a8da7395ad896696
Maddy Guthridge <hello@maddyguthridge.com>
2024-08-04 17:59:23 +1000

    Add test cases for getting/setting config

--- tests/backend/admin/config/get.test.ts
+++ tests/backend/admin/config/get.test.ts
@@ -3,6 +3,32 @@
  *
  * Returns the current site configuration.
  */
-import { it } from 'vitest';
+import api from '$endpoints';
+import { beforeEach, expect, it } from 'vitest';
+import { setup } from '../../helpers';
+import consts from '$lib/consts';
 
-it.todo('TODO');
+let token: string;
+
+beforeEach(async () => {
+  token = (await setup()).token;
+});
+
+it('Returns the current config contents', async () => {
+  await expect(api.admin.config.get(token)).resolves.toStrictEqual({
+    siteName: 'My Portfolio',
+    mainPageGroups: [],
+    version: consts.VERSION,
+  });
+});
+
+it('Errors if given an invalid token', async () => {
+  await expect(api.admin.config.get('invalid token'))
+    .rejects.toMatchObject({ code: 401 });
+});
+
+it("Errors if the data isn't set up", async () => {
+  await api.debug.clear();
+  await expect(api.admin.config.get('invalid token'))
+    .rejects.toMatchObject({ code: 400 });
+});
--- tests/backend/admin/config/put.test.ts
+++ tests/backend/admin/config/put.test.ts
@@ -3,6 +3,62 @@
  *
  * Allows users to edit the site configuration
  */
-import { it } from 'vitest';
+import { beforeEach, expect, it } from 'vitest';
+import { setup } from '../../helpers';
+import api from '$endpoints';
+import consts from '$lib/consts';
 
-it.todo('TODO');
+let token: string;
+
+beforeEach(async () => {
+  token = (await setup()).token;
+});
+
+it('Updates the current config contents', async () => {
+  await expect(api.admin.config.put(token, {
+    siteName: 'Name changed',
+    mainPageGroups: [],
+    version: consts.VERSION,
+  })).resolves.toStrictEqual({});
+  // Config should have updated
+  await expect(api.admin.config.get(token)).resolves.toStrictEqual({
+    siteName: 'Name changed',
+    mainPageGroups: [],
+    version: consts.VERSION,
+  });
+});
+
+it('Errors if the new config has an incorrect version', async () => {
+  await expect(api.admin.config.put(token, {
+    siteName: 'Name changed',
+    mainPageGroups: [],
+    version: consts.VERSION + 'invalid',
+  })).rejects.toMatchObject({ code: 400 });
+});
+
+it('Errors if the new config has references a non-existent page group', async () => {
+  await expect(api.admin.config.put(token, {
+    siteName: 'Name changed',
+    mainPageGroups: ['invalid'],
+    version: consts.VERSION,
+  })).rejects.toMatchObject({ code: 400 });
+});
+
+it.todo('Can set pages as a main page group');
+
+it('Rejects invalid tokens', async () => {
+  await expect(api.admin.config.put('invalid token', {
+    siteName: 'Name changed',
+    mainPageGroups: [],
+    version: consts.VERSION,
+  })).rejects.toMatchObject({ code: 401 });
+});
+
+it('Errors if site is not set up', async () => {
+  await api.debug.clear();
+  await expect(api.admin.config.put(token, {
+    siteName: 'Name changed',
+    mainPageGroups: [],
+    version: consts.VERSION,
+  })).rejects.toMatchObject({ code: 400 });
+});
