0b5d7f85ed9f7f4af3b09e099b906db38b4b6882
Maddy Guthridge <hello@maddyguthridge.com>
2024-08-19 22:08:40 +1000

    Migrate ID and name validation to helper functions

---
+++ src/lib/validators.ts
@@ -0,0 +1,49 @@
+import { error } from '@sveltejs/kit';
+
+/** Regex for matching group IDs */
+const idValidatorRegex = /^[a-z0-9-.]+$/;
+
+/** Ensure that the given ID string is valid */
+export function validateId(id: string): string {
+  if (!id.trim().length) {
+    return error(400, `Group ID '${id}' is empty`);
+  }
+  if (!idValidatorRegex.test(id)) {
+    return error(400, `Group ID '${id}' is contains illegal characters`);
+  }
+  if (id.startsWith('.')) {
+    return error(400, `Group ID '${id}' has a leading dot`);
+  }
+  if (id.endsWith('.')) {
+    return error(400, `Group ID '${id}' has a trailing dot`);
+  }
+  if (id.startsWith('-')) {
+    return error(400, `Group ID '${id}' has a leading dash`);
+  }
+  if (id.endsWith('-')) {
+    return error(400, `Group ID '${id}' has a trailing dash`);
+  }
+  return id;
+}
+
+/** Array of illegal characters that cannot be used in names */
+const illegalNameChars = ['\t', '\n', '\f', '\r'];
+
+export function validateName(name: string): string {
+  if (!name) {
+    return error(400, 'Name cannot be empty');
+  }
+  if (name.trim().length !== name.length) {
+    return error(400, 'Name cannot contain leading or trailing whitespace');
+  }
+  if (
+    illegalNameChars
+      .reduce((n, c) => n.replace(c, ''), name)
+      .length
+    !== name.length
+  ) {
+    return error(400, 'Name contains illegal whitespace characters');
+  }
+
+  return name;
+}
--- src/routes/api/group/[groupId]/+server.ts
+++ src/routes/api/group/[groupId]/+server.ts
@@ -1,9 +1,9 @@
 import { error, json } from '@sveltejs/kit';
-import { dataDirIsInit } from '$lib/server/data/dataDir';
 import { createGroup, deleteGroup, getGroupInfo, GroupInfoStruct, setGroupInfo } from '$lib/server/data/group';
 import { validateToken } from '$lib/server/auth';
 import { object, string, StructError, validate } from 'superstruct';
-import { getPortfolioGlobals, invalidatePortfolioGlobals } from '$lib/server/data/index.js';
+import { getPortfolioGlobals, invalidatePortfolioGlobals } from '$lib/server/data/index';
+import { validateId, validateName } from '$lib/validators';
 
 export async function GET({ params, request, cookies }) {
   const groupId = params.groupId;
@@ -18,79 +18,27 @@ export async function GET({ params, request, cookies }) {
   }
 }
 
-// TODO: Probably way more opportunities for optimising these functions by
-// using the cache of data
-
-/** Regex for matching group IDs */
-const groupIdValidator = /^[a-z0-9-.]+$/;
-
-const illegalNameChars = ['\t', '\n', '\f', '\r'];
-
 export async function POST({ params, request, cookies }) {
   const token = request.headers.get('Authorization');
   if (!token) {
     return error(401, 'Authorization token is required');
   }
 
-  await getPortfolioGlobals().catch(e => error(400, e));
+  const data = await getPortfolioGlobals().catch(e => error(400, e));
 
   await validateToken(token).catch(e => error(401, `${e}`));
 
   // Validate group ID
-  const groupId = params.groupId;
-
-  if (!groupId.trim().length) {
-    return error(400, `Group ID '${groupId}' is empty`);
-  }
-  if (!groupIdValidator.test(groupId)) {
-    return error(400, `Group ID '${groupId}' is contains illegal characters`);
-  }
-  if (groupId.startsWith('.')) {
-    return error(400, `Group ID '${groupId}' has a leading dot`);
-  }
-  if (groupId.endsWith('.')) {
-    return error(400, `Group ID '${groupId}' has a trailing dot`);
-  }
-  if (groupId.startsWith('-')) {
-    return error(400, `Group ID '${groupId}' has a leading dash`);
-  }
-  if (groupId.endsWith('-')) {
-    return error(400, `Group ID '${groupId}' has a trailing dash`);
-  }
+  const groupId = validateId(params.groupId);
 
   const [err, body] = validate(await request.json(), object({ name: string(), description: string() }));
   if (err) {
     return error(400, err);
   }
   const { name, description } = body;
+  validateName(name);
 
-  // Validate name
-  if (!name) {
-    return error(400, 'Name cannot be empty');
-  }
-  if (name.trim().length !== name.length) {
-    return error(400, 'Name cannot contain leading or trailing whitespace');
-  }
-  if (
-    illegalNameChars
-      .reduce((n, c) => n.replace(c, ''), name)
-      .length
-    !== name.length
-  ) {
-    return error(400, 'Name contains illegal whitespace characters');
-  }
-
-  let groupDataExists = false;
-  try {
-    await getGroupInfo(groupId);
-    groupDataExists = true;
-  } catch (e) {
-    // If it's a validation error, we should say so
-    if (e instanceof StructError) {
-      return error(400, `Group with ID ${groupId} already exists, but has invalid data`);
-    }
-  }
-  if (groupDataExists) {
+  if (data.groups[groupId]) {
     return error(400, `Group with ID ${groupId} already exists`);
   }
 
@@ -106,16 +54,14 @@ export async function PUT({ params, request, cookies }) {
     return error(401, 'Authorization token is required');
   }
 
-  await getPortfolioGlobals().catch(e => error(400, e));
+  const data = await getPortfolioGlobals().catch(e => error(400, e));
 
   await validateToken(token).catch(e => error(401, `${e}`));
 
   const groupId = params.groupId;
 
-  try {
-    await getGroupInfo(groupId);
-  } catch (e) {
-    return error(404, `Group with ID ${groupId} doesn't exist\n${e}`);
+  if (!data.groups[groupId]) {
+    return error(404, `Group with ID ${groupId} doesn't exist`);
   }
 
   const [err, info] = validate(await request.json(), GroupInfoStruct);
@@ -125,21 +71,7 @@ export async function PUT({ params, request, cookies }) {
   }
 
   // Validate name
-  const name = info.name;
-  if (!name) {
-    return error(400, 'Name cannot be empty');
-  }
-  if (name.trim().length !== name.length) {
-    return error(400, 'Name cannot contain leading or trailing whitespace');
-  }
-  if (
-    illegalNameChars
-      .reduce((n, c) => n.replace(c, ''), name)
-      .length
-    !== name.length
-  ) {
-    return error(400, 'Name contains illegal whitespace characters');
-  }
+  const name = validateName(info.name);
 
   // TODO: Other validation
 
@@ -155,16 +87,14 @@ export async function DELETE({ params, request, cookies }) {
     return error(401, 'Authorization token is required');
   }
 
-  await getPortfolioGlobals().catch(e => error(400, e));
+  const data = await getPortfolioGlobals().catch(e => error(400, e));
 
   await validateToken(token).catch(e => error(401, `${e}`));
 
   const groupId = params.groupId;
 
-  try {
-    await getGroupInfo(groupId);
-  } catch (e) {
-    return error(404, `Group with ID ${groupId} doesn't exist\n${e}`);
+  if (data.groups[groupId]) {
+    return error(404, `Group with ID ${groupId} doesn't exist`);
   }
 
   // Now delete the group
