2905ef9e386b1a54b5bfcdc23d40d7eccaaa8455
Maddy Guthridge <hello@maddyguthridge.com>
2024-08-30 19:16:50 +1000

    Update migration code to support new version

--- src/lib/server/data/migrations/index.ts
+++ src/lib/server/data/migrations/index.ts
@@ -1,5 +1,18 @@
+import { version } from '$app/environment';
+import { getConfig, setConfig } from '../config';
 import { getDataDir } from '../dataDir';
+import { getLocalConfig, setLocalConfig } from '../localConfig';
 import migrateFromV010 from './v0.1.0';
+
+/** Update config versions */
+async function updateConfigVersions() {
+  const config = await getConfig();
+  config.version = version;
+  await setConfig(config);
+  const configLocal = await getLocalConfig();
+  configLocal.version = version;
+  await setLocalConfig(configLocal);
+}
 
 /** Perform a migration from the given version */
 export default async function migrate(oldVersion: string) {
@@ -7,13 +20,18 @@ export default async function migrate(oldVersion: string) {
   try {
     if (oldVersion === '0.1.0') {
       await migrateFromV010(getDataDir());
+    } else if (oldVersion === '0.2.0') {
+      // FIXME: Need a nicer system for these kinds of version differences
+      // perhaps using semver?
+      await updateConfigVersions();
     } else {
-      console.log(`Migrate: unrecognised old version ${oldVersion}`);
-      process.exit(1);
+      const message = `Migrate: unrecognised old version ${oldVersion}`;
+      console.log(message);
+      throw new Error(message);
     }
   } catch (e) {
     console.log('!!! Error during migration');
     console.log(e);
-    process.exit(1);
+    throw e;
   }
 }
