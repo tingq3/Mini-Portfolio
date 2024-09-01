f64258e982f4ac60135e7c2f2315fe4491e7d1a9
Maddy Guthridge <hello@maddyguthridge.com>
2024-09-01 15:42:55 +1000

    Fix typescript

--- src/lib/blankConfig.ts
+++ src/lib/blankConfig.ts
@@ -4,6 +4,9 @@ import type { ConfigJson } from './server/data/config';
 
 const blankConfig: ConfigJson = {
   siteName: consts.APP_NAME,
+  siteShortName: consts.APP_NAME,
+  siteDescription: '',
+  siteKeywords: [],
   listedGroups: [],
   color: '#ffaaff',
   version,
