a19aecfd7057caa380c94a6c06e8524ebd89e37d
Maddy Guthridge <hello@maddyguthridge.com>
2024-09-12 22:37:05 +1000

    Implement git pull

--- src/routes/api/admin/repo/pull/+server.ts
+++ src/routes/api/admin/repo/pull/+server.ts
@@ -1,12 +1,21 @@
 import { validateTokenFromRequest } from '$lib/server/auth.js';
-import { getPortfolioGlobals } from '$lib/server/index.js';
+import { dataDirUsesGit, getDataDir } from '$lib/server/data/dataDir.js';
+import { getRepoStatus } from '$lib/server/git.js';
+import { getPortfolioGlobals, invalidatePortfolioGlobals } from '$lib/server/index.js';
 import { error, json } from '@sveltejs/kit';
+import simpleGit from 'simple-git';
 
 export async function POST({ request, cookies }) {
   await getPortfolioGlobals().catch(e => error(400, e));
   await validateTokenFromRequest({ request, cookies });
 
-  // TODO
+  if (!await dataDirUsesGit()) {
+    error(400, 'Data dir is not a git repo');
+  }
 
-  return json({}, { status: 200 });
+  const git = simpleGit(getDataDir());
+
+  await git.pull();
+  invalidatePortfolioGlobals();
+  return json(await getRepoStatus(), { status: 200 });
 }
--- tests/backend/admin/repo/pull.test.ts
+++ tests/backend/admin/repo/pull.test.ts
@@ -1,29 +1,49 @@
 /**
  * Test cases for POST /api/admin/repo/pull
  */
-import { it, expect, beforeEach } from 'vitest';
+import { it, expect, beforeEach, describe, vi } from 'vitest';
 import { setup } from '../../helpers';
-import makeClient, { type ApiClient } from '$endpoints';
 import gitRepos from '../../gitRepos';
 import simpleGit from 'simple-git';
 import { getDataDir } from '$lib/server/data/dataDir';
 import genTokenTests from '../../tokenCase';
+import makeClient, { type ApiClient } from '$endpoints';
 
-let api: ApiClient;
+// Git clone takes a while, increase the test timeout
+vi.setConfig({ testTimeout: 15_000 });
 
-beforeEach(async () => {
-  api = (await setup()).api;
+const OLD_COMMIT_HASH = 'd7ef6fd7ef9bac4c24f5634e6b1e76d201507498';
+
+it('Pulls latest changes', async () => {
+  const { api } = await setup(gitRepos.TEST_REPO_RW);
+  // Forcefully move back a number of commits, then invalidate the data
+  const git = simpleGit(getDataDir());
+  await git.reset(['--hard', OLD_COMMIT_HASH]);
+  await api.admin.data.refresh();
+  // Now git pull
+  await api.admin.repo.pull();
+  // The changes should have been applied
+  await expect(api.readme.get()).resolves.toStrictEqual({ readme: expect.stringContaining('Minifolio') });
 });
 
-it.todo('Pulls latest changes');
+it("Gives a 400 error when data dir isn't using git", async () => {
+  const { api } = await setup();
+  await expect(api.admin.repo.pull()).rejects.toMatchObject({ code: 400 });
+});
 
-it.todo('Latest changes are applied by invalidating globals');
+it('Gives a 400 when no data dir set up', async () => {
+  await expect(makeClient().admin.repo.pull()).rejects.toMatchObject({ code: 400 });
+});
 
-it.todo("Gives a 400 error when data dir isn't using git");
+describe('token tests', () => {
+  let api: ApiClient;
 
-it.todo('Gives a 400 when no data dir set up');
+  beforeEach(async () => {
+    api = (await setup()).api;
+  });
 
-genTokenTests(
-  () => api,
-  async api => {},
-);
+  genTokenTests(
+    () => api,
+    async api => api.admin.repo.pull(),
+  );
+});
