f11a7802694b75a1af689eddfb58c25fd258765b
Maddy Guthridge <hello@maddyguthridge.com>
2024-09-12 17:44:59 +1000

    Write tests for commit endpoint

--- tests/backend/admin/repo/commit.test.ts
+++ tests/backend/admin/repo/commit.test.ts
@@ -1,31 +1,63 @@
 /**
  * Test cases for POST /api/admin/repo/commit
  */
-import { it, expect, beforeEach } from 'vitest';
+import { it, expect, beforeEach, describe, vi } from 'vitest';
 import { setup } from '../../helpers';
-import makeClient, { type ApiClient } from '$endpoints';
-import gitRepos from '../../gitRepos';
+import gitRepos, { resetTestRepo } from '../../gitRepos';
 import simpleGit from 'simple-git';
 import { getDataDir } from '$lib/server/data/dataDir';
 import genTokenTests from '../../tokenCase';
+import apiClient, { type ApiClient } from '$endpoints';
 
-let api: ApiClient;
+// Git clone takes a while, increase the test timeout
+vi.setConfig({ testTimeout: 15_000 });
 
-beforeEach(async () => {
-  api = (await setup()).api;
+it('Creates a commit with the current changes', async () => {
+  const { api } = await setup(gitRepos.TEST_REPO_RW);
+  // Get current commit hash
+  const { repo: { commit } } = await api.admin.repo.get();
+  // Make some small change
+  await api.readme.set(`# Test repo\n\nUpdated at ${Date()}`);
+  const commitResult = await api.admin.repo.commit('Updated repo');
+  expect(commitResult).toMatchObject({
+    // One commit ahead of origin
+    ahead: 1,
+  });
+  expect(commitResult.commit).toStrictEqual(commit);
 });
 
-it.todo('Creates a commit with the current changes');
+it("Gives a 400 error when data dir isn't using git", async () => {
+  const { api } = await setup();
+  await expect(api.admin.repo.commit('Commit message')).rejects.toMatchObject({
+    code: 400,
+  });
+});
 
-it.todo('Returns that status is one commit ahead of origin');
+it('Gives a 400 when no data dir set up', async () => {
+  const api = apiClient();
+  await expect(api.admin.repo.commit('Commit message')).rejects.toMatchObject({
+    code: 400,
+  });
+});
 
-it.todo("Gives a 400 error when data dir isn't using git");
+it('Gives a 400 when there are no current changes', async () => {
+  const { api } = await setup(gitRepos.TEST_REPO_RW);
+  // Need to do an earlier commit because a data migration may have occurred
+  await api.admin.repo.commit('First commit').catch(() => {});
+  // Second commit fails as there are no changes
+  await expect(api.admin.repo.commit('Second commit')).rejects.toMatchObject({
+    code: 400,
+  });
+});
 
-it.todo('Gives a 400 when no data dir set up');
+describe('token test', () => {
+  let api: ApiClient;
+  beforeEach(async () => {
+    api = (await setup()).api;
+  });
 
-it.todo('Gives a 400 when there are no current changes');
-
-genTokenTests(
-  () => api,
-  async api => {},
-);
+  genTokenTests(
+    () => api,
+    async api => api.admin.repo.commit('Commit message'),
+  );
+});
