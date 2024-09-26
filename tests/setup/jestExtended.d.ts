1c0bbea4a3f9d5ac3c3044fda9f34fece5541e94
Maddy Guthridge <hello@maddyguthridge.com>
2024-09-26 17:42:27 +1000

    Fix type definitions for vitest jest-extended

--- tests/setup/jestExtended.d.ts
+++ tests/setup/jestExtended.d.ts
@@ -1,9 +1,12 @@
+// Need to use interfaces, as they don't register the extended type definitions
+// otherwise.
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 // https://jest-extended.jestcommunity.dev/docs/getting-started/setup#use-with-vitest
 import type CustomMatchers from 'jest-extended';
 import 'vitest';
 
 declare module 'vitest' {
-  type Assertion<T = any> = CustomMatchers<T>
-  type AsymmetricMatchersContaining<T = any> = CustomMatchers<T>
-  type ExpectStatic<T = any> = CustomMatchers<T>
+  interface Assertion<T = any> extends CustomMatchers<T> {}
+  interface AsymmetricMatchersContaining<T = any> extends CustomMatchers<T> {}
+  interface ExpectStatic<T = any> extends CustomMatchers<T> {}
 }
