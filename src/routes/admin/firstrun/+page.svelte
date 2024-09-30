c91a9210c23aabe3b452cfd9db6b03808e64df90
Maddy Guthridge <hello@maddyguthridge.com>
2024-09-30 23:41:57 +1000

    Dev qol: pre-fill the repo URL

--- src/routes/admin/firstrun/+page.svelte
+++ src/routes/admin/firstrun/+page.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
-  import type { FirstRunCredentials } from '$lib/server/auth';
-  import api from '$endpoints';
-  import Paper from '$components/Paper.svelte';
-  import Background from '$components/Background.svelte';
-  import Spinner from '$components/modals/Spinner.svelte';
-  import Error from '$components/modals/Error.svelte';
-  import Modal from '$components/modals/Modal.svelte';
-  import { goto } from '$app/navigation';
-  import Navbar from '$components/navbar';
-  import blankConfig from '$lib/blankConfig';
-  import CopyButton from '$components/CopyButton.svelte';
-  import consts from '$lib/consts';
+  import { dev } from "$app/environment";
+  import type { FirstRunCredentials } from "$lib/server/auth";
+  import api from "$endpoints";
+  import Paper from "$components/Paper.svelte";
+  import Background from "$components/Background.svelte";
+  import Spinner from "$components/modals/Spinner.svelte";
+  import Error from "$components/modals/Error.svelte";
+  import Modal from "$components/modals/Modal.svelte";
+  import { goto } from "$app/navigation";
+  import Navbar from "$components/navbar";
+  import blankConfig from "$lib/blankConfig";
+  import CopyButton from "$components/CopyButton.svelte";
+  import consts from "$lib/consts";
 
-  let repoUrl = '';
-  let repoBranch = '';
+  // In dev mode, use my portfolio data by default
+  let repoUrl = dev ? "git@github.com:MaddyGuthridge/portfolio-data.git" : "";
+  let repoBranch = "";
   let credentials: FirstRunCredentials | undefined;
 
   async function submitMain() {
@@ -39,33 +41,33 @@
   async function onSubmit(e: SubmitEvent) {
     const submitter = e.submitter?.id;
     switch (submitter) {
-      case 'submit-main':
+      case "submit-main":
         await submitMain();
         break;
-      case 'submit-no-git':
+      case "submit-no-git":
         await submitNoGit();
         break;
       default:
-        console.error('Submitter not recognised!');
+        console.error("Submitter not recognised!");
     }
   }
 
   let showLoading = false;
 
-  let errorText = '';
+  let errorText = "";
 </script>
 
 <svelte:head>
   <title>Setup - {consts.APP_NAME}</title>
-  <meta name="generator" content="{consts.APP_NAME}">
-  <meta name="theme-color" content="#aa00aa">
+  <meta name="generator" content={consts.APP_NAME} />
+  <meta name="theme-color" content="#aa00aa" />
   <!--
     Prevent web crawlers from indexing the firstrun page. Of course, if someone
     has an instance of this exposed to the open web without it being set up,
     that's absolutely on them, but the least we can do is stop instances that
     aren't set up from being easily searchable.
   -->
-  <meta name="robots" content="noindex">
+  <meta name="robots" content="noindex" />
 </svelte:head>
 
 <Background color="#aa00aa"></Background>
@@ -76,25 +78,38 @@
   <Paper>
     <main>
       <div class="center">
-        <h1 style="font-size: 3rem"> Welcome to your fancy new portfolio website! </h1>
+        <h1 style="font-size: 3rem">
+          Welcome to your fancy new portfolio website!
+        </h1>
         <h2>Let's get set up!</h2>
       </div>
 
       <form on:submit={onSubmit}>
         <h3>Data repository URL</h3>
-        <input type="text" id="repo-url" bind:value={repoUrl} placeholder="git@github.com:MaddyGuthridge/portfolio.git" />
+        <input
+          type="text"
+          id="repo-url"
+          bind:value={repoUrl}
+          placeholder="git@github.com:MaddyGuthridge/portfolio-data.git"
+        />
         <p>
-          It's a good idea to set up a repository to back up your portfolio data.
+          It's a good idea to set up a repository to back up your portfolio
+          data.
           <a
             href="https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-new-repository"
-            target="_blank"
-          >Create an empty git repository</a>
+            target="_blank">Create an empty git repository</a
+          >
           and enter the clone URL here. If you want to import existing data, enter
           your existing repository URL here.
         </p>
 
         <h3>Repository branch</h3>
-        <input type="text" id="repo-branch" bind:value={repoBranch} placeholder="main" />
+        <input
+          type="text"
+          id="repo-branch"
+          bind:value={repoBranch}
+          placeholder="main"
+        />
         <p>If you want to use a specific branch, you can enter it above.</p>
 
         <h3>Ready to get started?</h3>
@@ -102,24 +117,39 @@
 
         <h3>Don't want to use a git repo?</h3>
         <p>
-          Using a git repo is a great idea if you want your data to be  safely
+          Using a git repo is a great idea if you want your data to be safely
           backed up. But if you're just testing {consts.APP_NAME}, it's much
           quicker to get started without a git repo.
         </p>
-        <input type="submit" id="submit-no-git" value="I don't want to use git" />
+        <input
+          type="submit"
+          id="submit-no-git"
+          value="I don't want to use git"
+        />
       </form>
     </main>
   </Paper>
 </div>
 
 <!-- Spinner shows while setting up data dir -->
-<Spinner show={showLoading} header="Just a moment..." text="We're setting up your data" />
+<Spinner
+  show={showLoading}
+  header="Just a moment..."
+  text="We're setting up your data"
+/>
 
 <!-- Error shows if error occurs with setup -->
-<Error show={errorText !== ''} header="Oh no!" text={errorText} on:close={() => { errorText = ''; }} />
+<Error
+  show={errorText !== ""}
+  header="Oh no!"
+  text={errorText}
+  on:close={() => {
+    errorText = "";
+  }}
+/>
 
 {#if credentials}
-  <Modal show={true} on:close={() => goto('/')}>
+  <Modal show={true} on:close={() => goto("/")}>
     <h1 slot="header">Your data is set up!</h1>
     <p>This is your login information. Please ensure you do not lose it.</p>
     <!-- TODO: Add copy buttons for convenience -->
@@ -128,7 +158,9 @@
     </p>
     <div class="horizontal">
       Password: <b>{credentials.password}</b>
-      <CopyButton text={credentials.password} hint='Copy password'>Copy</CopyButton>
+      <CopyButton text={credentials.password} hint="Copy password"
+        >Copy</CopyButton
+      >
     </div>
   </Modal>
 {/if}
