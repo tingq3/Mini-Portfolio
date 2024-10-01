a2bcbf1e171d0e0cde7191888a2e6a1a71bdc7ce
Maddy Guthridge <hello@maddyguthridge.com>
2024-10-02 01:36:07 +1000

    Add test cases for public key endpoint

---
+++ tests/backend/admin/publickey/get.test.ts
@@ -0,0 +1,12 @@
+/**
+ * Test cases for public key requests
+ */
+
+import { describe, test } from "vitest";
+
+
+test.todo('Public key can be requested without token when server is not set up');
+
+test.todo('Public key can be requested with token when server is set up');
+
+describe.todo('Token tests');
