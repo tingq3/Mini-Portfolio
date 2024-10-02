9ef48abc2d43511bbf762a407d32520170785fcf
Maddy Guthridge <hello@maddyguthridge.com>
2024-10-02 17:26:01 +1000

    Implement basic key set-up

--- src/lib/server/keys.ts
+++ src/lib/server/keys.ts
@@ -1,12 +1,39 @@
 /** Code for managing the server's SSH keys */
-// import fs from 'fs/promises';
+import fs from 'fs/promises';
+import { getPrivateDataDir } from './data/dataDir';
+import { spawn } from 'child-process-promise';
+import { APP_NAME } from '$lib/consts';
 
-/** Generate an SSH key */
-export async function sshKeygen() {
+const KEY_TYPE = 'ed25519';
 
+const publicKeyFile = () => `${getPrivateDataDir()}/id_${KEY_TYPE}.pub`;
+const privateKeyFile = () => `${getPrivateDataDir()}/id_${KEY_TYPE}`;
+
+let publicKey: string | undefined;
+
+/** Returns the server's SSH public key */
+export async function getPublicKey(): Promise<string> {
+  if (publicKey) {
+    return publicKey;
+  }
+  publicKey = (await fs.readFile(publicKeyFile(), { encoding: 'utf-8' })).trim();
+  return publicKey;
 }
 
-// export async function getPublicKey(): Promise<string> {
-//   const data
-//   return fs.readFile()
-// }
+/** Regenerate the SSH key pair for the server */
+export async function regenerateKey(): Promise<string> {
+  publicKey = undefined;
+  await fs.unlink(privateKeyFile());
+  await fs.unlink(publicKeyFile());
+  await spawn('ssh-keygen', [
+    '-t',
+    KEY_TYPE,
+    '-f',
+    privateKeyFile(),
+    '-N',
+    '',
+    '-C',
+    `${APP_NAME} SSH key`,
+  ]);
+  return getPublicKey();
+}
