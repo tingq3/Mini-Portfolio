8c6abdfea64372c81c528e68f88a5e78813f2c4e
Maddy Guthridge <hello@maddyguthridge.com>
2024-10-02 17:01:41 +1000

    Fix linting for incomplete code

--- src/lib/server/keys.ts
+++ src/lib/server/keys.ts
@@ -1,12 +1,12 @@
 /** Code for managing the server's SSH keys */
-import fs from 'fs/promises';
+// import fs from 'fs/promises';
 
 /** Generate an SSH key */
 export async function sshKeygen() {
 
 }
 
-export async function getPublicKey(): Promise<string> {
-  const data
-  return fs.readFile()
-}
+// export async function getPublicKey(): Promise<string> {
+//   const data
+//   return fs.readFile()
+// }
