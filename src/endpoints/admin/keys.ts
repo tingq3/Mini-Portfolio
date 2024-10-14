28be1a3ed05f3ba01c5a3b227f3de92906cbc1ed
Maddy Guthridge <hello@maddyguthridge.com>
2024-10-14 16:04:55 +1100

    Mark failing tests for keys

--- src/endpoints/admin/keys.ts
+++ src/endpoints/admin/keys.ts
@@ -6,11 +6,45 @@ export default function keys(token: string | undefined) {
       'GET',
       '/api/admin/keys',
       token,
-    )) as Promise<{ publicKey: string }>;
+    )) as Promise<{ publicKey: string, keyPath: string }>;
   };
 
+  const setKeyPath = async (keyPath: string) => {
+    return json(apiFetch(
+      'POST',
+      '/api/admin/keys',
+      token,
+      { keyPath }
+    )) as Promise<{ publicKey: string, keyPath: string }>;
+  };
+
+  const disable = async () => {
+    return json(apiFetch(
+      'DELETE',
+      '/api/admin/keys',
+      token,
+    )) as Promise<Record<string, never>>;
+  }
+
+  const generate = async () => {
+    return json(apiFetch(
+      'POST',
+      '/api/admin/keys/generate',
+      token,
+    )) as Promise<{ publicKey: string, keyPath: string }>;
+  }
+
   return {
-    /** Returns the server's SSH public key */
+    /**
+     * Returns the server's SSH public key, and the path to the private key
+     * file
+     */
     get,
+    /** Sets the path to the file the server should use as the private key */
+    setKeyPath,
+    /** Disables SSH key-based authentication */
+    disable,
+    /** Generate an SSH key-pair */
+    generate,
   };
 }
--- tests/backend/admin/keys/get.test.ts
+++ tests/backend/admin/keys/get.test.ts
@@ -8,15 +8,30 @@ import { setup } from "../../helpers";
 import genTokenTests from "../../tokenCase";
 
 
-test('Public key can be requested without token when server is not set up', async () => {
+/**
+ * These first two tests fail due to the config.local.json not being generated
+ * until first-run.
+ *
+ * TODO: Fix this when splitting the setup process into stages.
+ */
+
+test('Public key is `null` before it is generated', { fails: true }, async () => {
   await expect(api().admin.keys.get())
-    .resolves.toStrictEqual({ publicKey: expect.any(String) });
+    .resolves.toStrictEqual({ publicKey: null });
+});
+
+
+test('Public key can be requested without token when server is not set up', { fails: true }, async () => {
+  await api().admin.keys.generate();
+  await expect(api().admin.keys.get())
+    .resolves.toStrictEqual({ publicKey: expect.any(String), keyPath: expect.any(String) });
 });
 
 test('Public key can be requested with token when server is set up', async () => {
   const { api } = await setup();
+  await api.admin.keys.generate();
   await expect(api.admin.keys.get())
-    .resolves.toStrictEqual({ publicKey: expect.any(String) });
+    .resolves.toStrictEqual({ publicKey: expect.any(String), keyPath: expect.any(String) });
 });
 
 describe('Token tests', () => {
