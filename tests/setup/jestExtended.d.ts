7fd10dee572329b702ee1ef1296a2620ff7a66b6
Maddy Guthridge <hello@maddyguthridge.com>
2024-11-17 00:15:30 +1100

    Revert changes to jestExtended.d.ts

--- tests/setup/jestExtended.d.ts
+++ tests/setup/jestExtended.d.ts
@@ -1,12 +1,18 @@
+/**
+ * Sets up Jest Extended to work with Vitest
+ *
+ * https://jest-extended.jestcommunity.dev/docs/getting-started/setup#use-with-vitest
+ */
+
 // Need to use interfaces, as they don't register the extended type definitions
 // otherwise.
- 
-// https://jest-extended.jestcommunity.dev/docs/getting-started/setup#use-with-vitest
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import type CustomMatchers from 'jest-extended';
 import 'vitest';
 
 declare module 'vitest' {
-  type Assertion<T = any> = {} & CustomMatchers<T>
-  type AsymmetricMatchersContaining<T = any> = {} & CustomMatchers<T>
-  type ExpectStatic<T = any> = {} & CustomMatchers<T>
+  interface Assertion<T = any> extends CustomMatchers<T> { }
+  interface AsymmetricMatchersContaining<T = any> extends CustomMatchers<T> { }
+  interface ExpectStatic<T = any> extends CustomMatchers<T> { }
 }
